b0b064822eae1d4122b8ccc9687ecd833b74a4a5
Michael Rosata <mrosata1984@gmail.com>
2016-07-25 19:32:16 -0400

    File upload works

--- app/js/file-uploader.js
+++ app/js/file-uploader.js
@@ -5,6 +5,7 @@ export default class FireBaseStorage {
   constructor(config) {
     // Not sure if this will need config.
     this.config = config || {};
+    this.files = [];
   }
 
   getStorageReference() {
@@ -15,27 +16,50 @@ export default class FireBaseStorage {
     return this.getStorageReference().child(fullFilePath);
   }
 
+  // This is just static version of above
+  static getRef() {
+    return firebase.storage().ref();
+  }
+
+
   getFileInfo(fullFilePath) {
     const file = this.getFileReference(fullFilePath);
     return (file && is.isObject(file)) ? file : null;
   }
-  
+
+  static putFile(file, callback) {
+    console.log(file);
+
+    let uploadTask = FireBaseStorage.getRef().child(file.name).put(file);
+
+    uploadTask.on('state_changed', function(snapshot){
+      // Observe state change events such as progress, pause, and resume
+      // See below for more detail
+      console.log('State changed from fb!', snapshot);
+    }, function(error) {
+      // Handle unsuccessful uploads
+      console.error('ERROR ERROR!', error);
+    }, function() {
+      // Handle successful uploads on complete
+      // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+      callback(uploadTask.snapshot.downloadURL);
+    });
+  }
+
+
   static getFileFromInput(evt, callback) {
 
     let preview = document.querySelector('img');
     let file = document.querySelector('input[type=file]').files[0];
     let reader = new FileReader();
 
-    
-    
     reader.addEventListener("load", function () {
       preview.src = reader.result;
-      callback(reader);
+
     }, false);
 
     if (file) {
-      reader.readAsDataURL(file);
-      console.log(file);
+      FireBaseStorage.putFile(file, callback);
     }
   }
 
@@ -43,7 +67,6 @@ export default class FireBaseStorage {
     const fileInput = document.querySelector('input[type=file]');
     fileInput.onchange = function (evt) {
       FireBaseStorage.getFileFromInput(evt, callback);
-      
     }
   }
 }
