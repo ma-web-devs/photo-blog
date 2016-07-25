d269eaf2977277b2c2a1670e9dcd68be4409a683
AbigailMathews <abbymathews@gmail.com>
2016-07-25 17:03:55 -0400

    Initialize firebase app (main.js)

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,21 @@
 /**
- * Created by michael on 7/25/16.
+ * Created by ma-web-devs on 7/25/16.
  */
+
+// Load our API keys
+javascript var keys = require('api-key.js');
+ 
+var firebase = require("firebase/app");
+    require("firebase/auth");
+    require("firebase/database");
+    require("firebase/storage");
+
+var config = {
+    apiKey: keys.firebase_key,
+    authDomain: "photoblog-11d60.firebaseapp.com",
+    databaseURL: "https://photoblog-11d60.firebaseio.com",
+    storageBucket: "photoblog-11d60.appspot.com",
+};
+
+firebase.initializeApp(config);
+
